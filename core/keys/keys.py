3992250ce6ae73b2746a5b92e947b1f9c7443c02
Amy Brennan-Luna <codesaway@gmail.com>
2024-07-04 05:42:30 -0500

    Create text_codesaway capture

---
+++ core/keys/keys.py
@@ -0,0 +1,8 @@
+from talon import Module
+
+mod = Module()
+
+mod.list(
+    "key_punctuation_codesaway",
+    "Symbols for inserting punctuation into CodesAway text",
+)
--- core/text/text_and_dictation.py
+++ core/text/text_and_dictation.py
@@ -1,22 +1,22 @@
 # Reference: https://github.com/AndreasArvidsson/andreas-talon/blob/master/core/text/text_and_dictation.py
 
-from talon import Context, Module
+from talon import Module
 
-from ...community.core.text.text_and_dictation import format_phrase
+from ....community.core.text.text_and_dictation import format_phrase
 
 mod = Module()
 
-ctx = Context()
-ctx.matches = r"""
-tag: user.enable_andreas_talon
-"""
 
-# ----- Captures used in both command and dictation mode -----
+# Tweaked from original
+@mod.capture(rule="spell {user.letter}+")
+def spell(m) -> str:
+    """Spell word phoneticly"""
+    return "".join(m.letter_list)
 
 
 text_rule_parts = [
     "{user.vocabulary}",
-    # "{user.key_punctuation}", # Commented out (for now at least)
+    "{user.key_punctuation_codesaway}",
     "<user.abbreviation>",
     "<user.spell>",
     "<user.number_prefix>",
@@ -26,10 +26,7 @@ text_rule_parts = [
 text_rule = f"({'|'.join(text_rule_parts)})+"
 
 
-@ctx.capture("user.text", rule=text_rule)
-def text(m) -> str:
+@mod.capture(rule=text_rule)
+def text_codesaway(m) -> str:
     """Mixed words, numbers and punctuation, including user-defined vocabulary, abbreviations and spelling."""
     return format_phrase(m)
-
-
-# TODO: add text_code and phrase (in original, but omitted in this copy)
