20304333b61224a4cb1051ffe81c138e5ff108f1
Amy Brennan-Luna <codesaway@gmail.com>
2024-07-26 12:46:12 -0500

    Initial code for Talon Adventure Game (TAG) to facilitate learning Talon Voice commands

--- talon_adventure_game/talon_adventure_game.py
+++ talon_adventure_game/talon_adventure_game.py
@@ -1,22 +1,41 @@
 from talon import Module, cron, ui
 from talon.canvas import Canvas
-from talon.skia import Rect
+from talon.skia import Paint, Rect
 from talon.skia.canvas import Canvas as SkiaCanvas
 
 mod = Module()
 
 
+def calculate_bottom_border(paint: Paint):
+    """Calculates bottom border used for letters which appear below baseline"""
+    # Workaround for paint.measure_text, which doesn't factor this in
+    letters = ["g", "j", "p", "q", "y"]
+    bottom_border = 0
+
+    for letter in letters:
+        rect: Rect = paint.measure_text(letter)[1]
+        bottom_border = max(bottom_border, rect.y + rect.height)
+
+    return bottom_border
+
+
 @mod.action_class
 class Actions:
-    def tag_game_show_text(screen: ui.Screen, text: str):
+    def tag_game_show_center_text(screen: ui.Screen, text: str):
         """Shows text for tag game in center of screen"""
 
         # Reference: community\core\screens\screens.py -> show_screen_number,
         def on_draw(c: SkiaCanvas):
-            c.paint.typeface = "arial"
+            paint: Paint = c.paint
+
+            paint.typeface = "arial"
             # The min(width, height) is to not get gigantic size on portrait screens
-            c.paint.textsize = round(min(c.width, c.height) / 8)
-            rect: Rect = c.paint.measure_text(text)[1]
+            paint.textsize = round(min(c.width, c.height) / 8)
+
+            rect: Rect
+            something, rect = paint.measure_text(text)
+            bottom_border = calculate_bottom_border(paint)
+
             x = c.x + c.width / 2 - rect.x - rect.width / 2
             y = c.y + c.height / 2 + rect.height / 2
 
@@ -26,15 +45,15 @@ class Actions:
                 x - border_size,
                 y - rect.height - border_size,
                 rect.width + 2 * border_size,
-                rect.height + 2 * border_size,
+                rect.height + 2 * border_size + bottom_border,
             )
 
-            c.paint.style = c.paint.Style.FILL
-            c.paint.color = "663399"  # Rebecca purple
+            paint.style = paint.Style.FILL
+            paint.color = "663399"  # Rebecca purple
             c.draw_rect(card_rect)
 
-            c.paint.style = c.paint.Style.FILL
-            c.paint.color = "33b2cd"  # Amy's blue color
+            paint.style = paint.Style.FILL
+            paint.color = "33b2cd"  # Amy's blue color
             c.draw_text(text, x, y)
 
             cron.after("3s", canvas.close)
